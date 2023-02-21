7ffdba12c9d7390ebd2191c6dafd503036c59b08
Kien Duong <kienduong1296@gmail.com>
2023-02-21 21:27:21 +0700

    config tailwind and hero section

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const HeroImage = '../images/heroimg.png';
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				transparent: 'transparent',
+				primary: '#31376E',
+				secondary: '#4482B2',
+				light: '#ffffff',
+				dark: '#333333'
+			},
+		},
+	},
+	plugins: [],
+}
