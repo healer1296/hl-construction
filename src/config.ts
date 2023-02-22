68d47c696f764954dc0fa8a5e5b596d3e67e2cdc
Kien Duong <kienduong1296@gmail.com>
2023-02-22 13:08:39 +0700

    service section

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@ export const Logo = '';
 export const HeaderPhone = '../images/svg/header-phone.svg';
 export const HeaderEmail = '../images/svg/header-email.svg';
 export const FooterLogo = '../images/footer-logo.png';
+export const ServiceImage = ''
