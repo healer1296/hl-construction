5d3358c7b01b92103d12a4a1b70ed12a36142199
Kien Duong <kienduong1296@gmail.com>
2023-02-22 14:32:23 +0700

    add project section

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,35 @@ export const Logo = '';
 export const HeaderPhone = '../images/svg/header-phone.svg';
 export const HeaderEmail = '../images/svg/header-email.svg';
 export const FooterLogo = '../images/footer-logo.png';
-export const ServiceImage = ''
+
+export const ServiceImage1 = '../images/service1.png';
+export const ServiceImage2 = '../images/service2.png';
+export const ServiceImage3 = '../images/service3.png';
+export const ServiceImage4 = '../images/service4.png';
+
+export const ProjectImage = [
+    {
+        src: '../images/project1.jpeg',
+        alt: 'Project Image'
+    },
+    {
+        src: '../images/project2.jpeg',
+        alt: 'Project Image'
+    },
+    {
+        src: '../images/project3.jpeg',
+        alt: 'Project Image'
+    },
+    {
+        src: '../images/project3.jpeg',
+        alt: 'Project Image'
+    },
+    {
+        src: '../images/project2.jpeg',
+        alt: 'Project Image'
+    },
+    {
+        src: '../images/project1.jpeg',
+        alt: 'Project Image'
+    }
+];
