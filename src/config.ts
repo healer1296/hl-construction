b6abe1004a30b4631ef229368ff27fd5d86b74cc
Kien Duong <kienduong1296@gmail.com>
2023-03-20 23:44:27 +0700

    change something

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const Logo = '';
+export const Logo = '../images/svg/logo.svg';
 export const HeaderPhone = '../images/svg/header-phone.svg';
 export const HeaderEmail = '../images/svg/header-email.svg';
 export const FooterLogo = '../images/footer-logo.png';
